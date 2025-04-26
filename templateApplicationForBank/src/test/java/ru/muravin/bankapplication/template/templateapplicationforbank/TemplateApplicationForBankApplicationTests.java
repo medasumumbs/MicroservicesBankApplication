c064d4dfc5d5169d8aa09bada62ad030ca572dcf
medasu <semen.muravin@gmail.com>
2025-04-26 11:50:59 +0300

    Инициализация проекта

---
+++ templateApplicationForBank/src/test/java/ru/muravin/bankapplication/template/templateapplicationforbank/TemplateApplicationForBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.muravin.bankapplication.template.templateapplicationforbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationForBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
