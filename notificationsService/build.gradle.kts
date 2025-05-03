ba02ea72a58be62884d889404482c57fdfdf9316
medasu <semen.muravin@gmail.com>
2025-05-03 22:00:17 +0300

    Реализован паттерн ServiceDiscovery с помощью Eureka

--- notificationsService/build.gradle.kts
+++ notificationsService/build.gradle.kts
@@ -31,7 +31,8 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     implementation("org.springframework.cloud:spring-cloud-starter-config:4.2.2")  // Для взаимодействия с Config Server
     implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp:4.2.1")// Для работы Spring Cloud Bus c RabbitM
-
+// https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-netflix-eureka-client
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.2.1")
     compileOnly("org.projectlombok:lombok")
 
     runtimeOnly("org.postgresql:postgresql")
--- notificationsService/src/main/java/ru/muravin/bankapplication/notificationsService/controller/TestController.java
+++ notificationsService/src/main/java/ru/muravin/bankapplication/notificationsService/controller/TestController.java
@@ -1,17 +1,26 @@
 package ru.muravin.bankapplication.notificationsService.controller;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.bus.BusProperties;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
-@RequestMapping("/texts")
+@RequestMapping
 @RefreshScope
 public class TestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     // Property из Config Server, которое может быть автоматически обновлено
     @Value("${text-to-print}")
@@ -25,11 +34,22 @@ public class TestController {
         this.busProps = busProps;
     }
 
-    @GetMapping
+    @GetMapping("/texts")
     public String getText() {
         var message = busProps.getId() + ":\n" + textToPrint;
         System.out.println(message);
         return message;
+    }
+
+    @GetMapping("/discoveredServicesList")
+    public Map<String,String> discoveredServicesList() {
+        var response = new HashMap<String, String>();
+        AtomicInteger index = new AtomicInteger();
+        discoveryClient.getServices().stream().forEach(s -> {
+            index.getAndIncrement();
+            response.put(index.toString(), s);
+        });
+        return response;
     }
 
     @PostConstruct
