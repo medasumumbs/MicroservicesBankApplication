8bdcebed16b2472927a15111788743788e60322f
sem <sem@finstream.ru>
2025-05-22 12:34:26 +0300

    Исправлены тесты для exchangegeneratorservice

--- exchange_generator_service/src/test/java/ru/muravin/bankapplication/notificationsService/serviceTest/ServiceTests.java
+++ exchange_generator_service/src/test/java/ru/muravin/bankapplication/notificationsService/serviceTest/ServiceTests.java
@@ -1,6 +1,7 @@
 package ru.muravin.bankapplication.notificationsService.serviceTest;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
@@ -37,10 +38,7 @@ public class ServiceTests {
 
     @Test
     void testCreateRandomFloat_shouldReturnValidValue() {
-        // Act
         Mono<Float> result = RatesGenerationService.createRandomFloat();
-
-        // Assert
         StepVerifier.create(result)
                 .expectNextCount(1)
                 .verifyComplete();
@@ -51,87 +49,70 @@ public class ServiceTests {
 
     @Test
     void testGenerateAndSendRates_shouldSendTwoCurrencyRates() {
-        // Arrange
         var randomFloat = 12345f;
 
-        // Mock WebClient chain: post -> uri -> body -> retrieve
-        WebClient.RequestBodySpec requestBodySpec = mock(WebClient.RequestBodyUriSpec.class);
+        WebClient.RequestBodySpec requestBodySpec = mock(WebClient.RequestBodySpec.class);
         WebClient.RequestBodyUriSpec requestBodyUriSpec = mock(WebClient.RequestBodyUriSpec.class);
-        WebClient.RequestHeadersSpec<?> requestHeadersSpec = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.RequestHeadersSpec requestHeadersSpec = mock(WebClient.RequestHeadersSpec.class);
         WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
 
         when(webClientBuilder.build()).thenReturn(mockWebClient);
         when(mockWebClient.post()).thenReturn(requestBodyUriSpec);
-        ///when(requestBodyUriSpec.uri(any(URI.class))).thenReturn()
-
-
-        when(mockWebClient.post()).thenReturn((WebClient.RequestBodyUriSpec) requestBodySpec);
-        when(requestBodySpec.uri("http://gateway/currencyExchangeService/rates")).thenReturn(requestHeadersSpec);
-        when(requestHeadersSpec.body(any())).thenReturn(requestHeadersSpec); // <-- body используется после uri
+        when(requestBodyUriSpec.uri(any(String.class))).thenReturn(requestBodySpec);
+        when(requestBodySpec.bodyValue(any())).thenReturn(requestHeadersSpec);
         when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
         when(responseSpec.bodyToMono(HttpResponseDto.class)).thenReturn(Mono.just(new HttpResponseDto("200", "OK")));
 
         try (var mocked = Mockito.mockStatic(RatesGenerationService.class)) {
             mocked.when(() -> RatesGenerationService.createRandomFloat()).thenReturn(Mono.just(randomFloat));
 
-            // Act
             ratesGenerationService.generateAndSendRates();
-
-            // Assert
             verify(mockWebClient, times(1)).post();
-            verify(requestBodySpec, times(1)).uri("http://gateway/currencyExchangeService/rates");
-            verify(requestHeadersSpec, times(1)).body(any());
+            verify(requestBodyUriSpec, times(1)).uri("http://gateway/currencyExchangeService/rates");
+            verify(requestHeadersSpec, times(1)).retrieve();
             verify(responseSpec, times(1)).bodyToMono(HttpResponseDto.class);
         }
     }
 
     @Test
     void testGenerateAndSendRates_shouldHandleServerError() {
-        // Arrange
         var randomFloat = 12345f;
-
         WebClient.RequestBodySpec requestBodySpec = mock(WebClient.RequestBodySpec.class);
-        WebClient.RequestHeadersSpec<?> requestHeadersSpec = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.RequestBodyUriSpec requestBodyUriSpec = mock(WebClient.RequestBodyUriSpec.class);
+        WebClient.RequestHeadersSpec requestHeadersSpec = mock(WebClient.RequestHeadersSpec.class);
         WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
-
-        when(mockWebClient.post()).thenReturn(requestBodySpec);
-        when(requestBodySpec.uri("http://gateway/currencyExchangeService/rates")).thenReturn(requestHeadersSpec);
-        when(requestHeadersSpec.body(any())).thenReturn(requestHeadersSpec);
+        when(webClientBuilder.build()).thenReturn(mockWebClient);
+        when(mockWebClient.post()).thenReturn(requestBodyUriSpec);
+        when(requestBodyUriSpec.uri(any(String.class))).thenReturn(requestBodySpec);
+        when(requestBodySpec.bodyValue(any())).thenReturn(requestHeadersSpec);
         when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
-        when(responseSpec.bodyToMono(HttpResponseDto.class))
-                .thenReturn(Mono.error(new WebClientResponseException(HttpStatus.INTERNAL_SERVER_ERROR, "Error", null, null, null)));
-
-        // Replace createRandomFloat to return fixed value
+        when(responseSpec.bodyToMono(HttpResponseDto.class)).thenReturn(
+                Mono.error(new WebClientResponseException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error", null, null, null)));
         try (var mocked = Mockito.mockStatic(RatesGenerationService.class)) {
             mocked.when(() -> RatesGenerationService.createRandomFloat()).thenReturn(Mono.just(randomFloat));
-
-            // Act
             ratesGenerationService.generateAndSendRates();
-
-            // Assert
             verify(responseSpec, times(1)).bodyToMono(HttpResponseDto.class);
         }
     }
 
     @Test
     void testGenerateAndSendRates_shouldHandleUnknownError() {
-        // Arrange
         var randomFloat = 12345f;
 
         WebClient.RequestBodySpec requestBodySpec = mock(WebClient.RequestBodySpec.class);
-        WebClient.RequestHeadersSpec<?> requestHeadersSpec = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.RequestBodyUriSpec requestBodyUriSpec = mock(WebClient.RequestBodyUriSpec.class);
+        WebClient.RequestHeadersSpec requestHeadersSpec = mock(WebClient.RequestHeadersSpec.class);
         WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
 
-        when(mockWebClient.post()).thenReturn(requestBodySpec);
-        when(requestBodySpec.uri("http://gateway/currencyExchangeService/rates")).thenReturn(requestHeadersSpec);
-        when(requestHeadersSpec.body(any())).thenReturn(requestHeadersSpec);
+        when(webClientBuilder.build()).thenReturn(mockWebClient);
+        when(mockWebClient.post()).thenReturn(requestBodyUriSpec);
+        when(requestBodyUriSpec.uri(any(String.class))).thenReturn(requestBodySpec);
+        when(requestBodySpec.bodyValue(any())).thenReturn(requestHeadersSpec);
         when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
-        when(responseSpec.bodyToMono(HttpResponseDto.class))
-                .thenReturn(Mono.error(new RuntimeException("Unknown error")));
-
+        when(responseSpec.bodyToMono(HttpResponseDto.class)).thenReturn(
+                Mono.error(new RuntimeException("Unknown error")));
         try (var mocked = Mockito.mockStatic(RatesGenerationService.class)) {
             mocked.when(() -> RatesGenerationService.createRandomFloat()).thenReturn(Mono.just(randomFloat));
-
             ratesGenerationService.generateAndSendRates();
             verify(responseSpec, times(1)).bodyToMono(HttpResponseDto.class);
         }
