e6cb9232a1392abcea1e1eb5aea7728d501538cf
medasu <semen.muravin@gmail.com>
2025-06-11 22:59:56 +0300

    Исправлены тесты с Embedded Kafka для Consumer'а в Currency Rates Service

--- currency_exchange_service/src/test/java/ru/muravin/bankapplication/currencyExchangeService/kafkaIntegrationTest/CurrencyRatesConsumerTest.java
+++ currency_exchange_service/src/test/java/ru/muravin/bankapplication/currencyExchangeService/kafkaIntegrationTest/CurrencyRatesConsumerTest.java
@@ -1,8 +1,10 @@
 package ru.muravin.bankapplication.currencyExchangeService.kafkaIntegrationTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.MockConsumer;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.Deserializer;
@@ -11,9 +13,13 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.*;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -23,14 +29,18 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.EmbeddedKafkaKraftBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import ru.muravin.bankapplication.currencyExchangeService.CurrencyExchangeServiceApplication;
@@ -53,6 +63,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
@@ -65,103 +76,72 @@ import static org.mockito.Mockito.verify;
         "spring.jpa.hibernate.ddl-auto=update",
         "spring.liquibase.enabled=false",
         "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration," +
-                "org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration"
+                "org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration",
+        "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
 },
         classes = {CurrencyExchangeServiceApplication.class, CurrencyRatesConsumer.class,})
 @TestPropertySource(locations = "classpath:application.yml")
-@EmbeddedKafka(partitions = 1, topics = { "currency-rates" }, brokerProperties = {
-        "key.serializer=org.apache.kafka.common.serialization.StringSerializer",
-        "value.serializer=org.springframework.kafka.support.serializer.JsonSerializer"
-})
-public class CurrencyRatesConsumerIntegrationTest {
-
+@EmbeddedKafka(
+        partitions = 1,
+        topics = "currency-rates",
+        brokerProperties = {
+                "listeners=PLAINTEXT://localhost:9092",
+                "port=9092"
+        }
+)
+@EnableKafka
+class CurrencyRatesConsumerTest {
     @MockitoBean
     private OAuth2SecurityConfig oAuth2SecurityConfig;
 
     @MockitoBean
     private JwtAuthenticationConverter jwtAuthenticationConverter;
-    @Autowired
+    private static EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @MockitoBean
     private CurrencyRatesService currencyRatesService;
 
-    private KafkaMessageListenerContainer<String, Object> container;
-
-    private MockConsumer mockConsumer;
-
-    @Autowired
-    CurrencyRatesConsumer currencyRatesConsumer;
-    @Autowired
-    ApplicationContext applicationContext;
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaMessageListenerContainer<String, String> container;
+    private BlockingQueue<ConsumerRecord<String, String>> records;
 
 
-    @BeforeEach
-    void setUp() {
-        // 1. Создаем ConsumerFactory с минимальными настройками
-        Map<String, Object> props = new HashMap<>();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "currency-rates-grp");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.springframework.kafka.support.serializer.JsonDeserializer");
-
-        ConsumerFactory<String, Object> consumerFactory =
-                new DefaultKafkaConsumerFactory<>(props);
-
-
-        // 2. Создаем реальный Kafka Consumer
-        Deserializer<String> keyDeserializer = new StringDeserializer();
-        Deserializer<Map> valueDeserializer = new JsonDeserializer();
-
-        mockConsumer = new MockConsumer<>(InMemoryOffsetBacking, keyDeserializer, valueDeserializer);
-
-        // 3. Настраиваем контейнер
-        ContainerProperties containerProps = new ContainerProperties("currency-rates");
-        container = new KafkaMessageListenerContainer<>(consumerFactory, containerProps);
-        container.setupMessageListener((MessageListener<String, Object>) record -> {
-            List<LinkedHashMap> list = Collections.singletonList((LinkedHashMap) record.value());
-            Acknowledgment ack = mock(Acknowledgment.class);
-            currencyRatesConsumer.consume(list, ack);
-        });
-
-        container.start();
-        ContainerTestUtils.waitForAssignment(container, 1); // Ждем партиционное назначение
+    @BeforeAll
+    static void setUp() {
+        embeddedKafkaBroker = new EmbeddedKafkaKraftBroker(1, 1, "currency-rates");
+        embeddedKafkaBroker.kafkaPorts(9092); // указываем порт
+        embeddedKafkaBroker.afterPropertiesSet();   // инициализация
     }
 
-    @AfterEach
-    void tearDown() {
-        if (container != null) {
-            container.stop();
-        }
+    @DynamicPropertySource
+    static void configureKafka(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", () -> "localhost:9092");
     }
 
     @Test
-    void testConsume_ShouldProcessAndSaveCurrencyRates() {
-        // Given
-        LinkedHashMap<String, Object> message = new LinkedHashMap<>();
-        message.put("buyRate", 1.12);
-        message.put("sellRate", 1.15);
-        message.put("currencyCode", "USD");
+    void testConsume_ShouldCallSaveRates_WhenMessageReceived() {
+        // GIVEN
+        KafkaTemplate<String, Object> kafkaTemplate = new KafkaTemplate<>(
+                new DefaultKafkaProducerFactory<>(kafkaTemplateProps())
+        );
 
-        ConsumerRecord<String, Object> record = new ConsumerRecord<>("currency-rates", 0, 0L, null, message);
+        // WHEN
+        List<CurrencyRateDto> payload = List.of(
+                new CurrencyRateDto("USD", 75.1f, 76.1f),
+                new CurrencyRateDto("EUR", 90.0f, 91.0f)
+        );
+        kafkaTemplate.send("currency-rates", payload);
+        kafkaTemplate.flush();
 
-        // When
-        mockConsumer.addRecord(record);
-        mockConsumer.poll(1000);
-
-        // Then
-        verify(currencyRatesService, timeout(1000).times(1)).saveRates(anyList());
+        // THEN
+        verify(currencyRatesService, timeout(5000).times(1)).saveRates(anyList());
     }
-    class NoOpOffsetBackingStore implements BackingStore {
-        @Override
-        public void start() {}
 
-        @Override
-        public void stop() {}
-
-        @Override
-        public void commit(Map<TopicPartition, Long> offsets) {}
-
-        @Override
-        public Map<TopicPartition, Long> read() {
-            return new HashMap<>();
-        }
+    private Map<String, Object> kafkaTemplateProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
     }
 }
